14bf3a0db4d8ee2a632ecccde3c39a6726c022b9
JStudios6118 <thisisjstudios@gmail.com>
2024-11-13 10:06:42 -0800

    Cool demo

--- example/node1.cpp
+++ example/node1.cpp
@@ -2,13 +2,19 @@
 
 #include "../lib/Fjord.h"
 
+Node square = engine.createObject();
+
 bool Engine::initObjs(){// define initObjs
     engine.createObject();
     return true;
 }
 
+int direction = 1;
+
 void Node::update(){ // update stuff
-    printf("helpmee%d\n", position.x);
-    position.x += 1;
+    if (engine.nodes[0].position.x > 500){
+        direction = -1;
+    }
+    engine.nodes[0].position.x += direction;
 }
 
