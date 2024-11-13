f92abd19c94f3729d52b36576fc192b24ce8e5be
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-11-13 10:09:07 -0800

    frick

--- example/node1.cpp
+++ example/node1.cpp
@@ -2,13 +2,22 @@
 
 #include "../lib/Fjord.h"
 
+class Node1 : public Node{ // Base Class of Every Node
+    public:
+        Node1(int xPos, int yPos, int width, int height, int zIndex);// add constructor with vars for rendering
+        void update(); // update function
+        Vector2 position = Vector2(0, 0);
+        Vector2 scale = Vector2(50, 50);
+        int zIndex; // create vars that go in rect
+        SDL_Rect rect; // sdl rect for rendering
+};
+
 bool Engine::initObjs(){// define initObjs
     engine.createObject();
     return true;
 }
 
-void Node::update(){ // update stuff
-    printf("helpmee%d\n", position.x);
+void Node1::update(){ // update stuff
     position.x += 1;
 }
 
