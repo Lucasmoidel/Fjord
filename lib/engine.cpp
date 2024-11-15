abbf560a9d068dfd53d771178fa5fdf64666733a
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-11-14 23:00:28 -0800

    bye bye warning

--- lib/engine.cpp
+++ lib/engine.cpp
@@ -17,8 +17,9 @@ void Engine::update(){
 
     Time::deltaTime =  (SDL_GetTicks() - Time::last_frame_time) / 1000.0f;
     Time::last_frame_time = SDL_GetTicks();
-    for (int i = 0; i < engine.nodes.size(); i++){
+    for (size_t i = 0; i < engine.nodes.size(); i++){
         nodes[i]->Update();
+        nodes[i]->rect = {(int)nodes[i]->transform.position.x, (int)nodes[i]->transform.position.y, 200, 200};
     }
 }
 
@@ -26,8 +27,7 @@ void Engine::render(){ // render objects
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //sets background color
     SDL_RenderClear(renderer); // clear screen
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // sets object color
-    for (int i = 0; i < engine.nodes.size(); i++){
-        nodes[i]->rect = {(int)nodes[i]->transform.position.x, (int)nodes[i]->transform.position.y, 200, 200};
+    for (size_t i = 0; i < engine.nodes.size(); i++){
         SDL_RenderDrawRect(renderer, &nodes[i]->rect);
     }
     SDL_RenderPresent(renderer); // shows render changes
