f4d2b9adadf4eec7f58cdf26968b6dc1c81858a6
Lucas Moidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-11-28 19:12:12 -0800

    Update utilities.h

--- lib/utilities.h
+++ lib/utilities.h
@@ -2,8 +2,8 @@
 #define UTILITIES_H
 
 namespace Utilities {
-    void ClampI(int* numberToClamp,int minValue, int maxValue);
-    void ClampF(float* numberToClamp,float minValue, float maxValue);
+    void ClampI(int numberToClamp,int minValue, int maxValue);
+    void ClampF(float numberToClamp,float minValue, float maxValue);
 }
 
 #endif
