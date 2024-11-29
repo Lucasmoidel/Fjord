60f3303d556d89db3e8aee3bfd5e7b39e134e414
Lucas Moidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-11-28 19:11:58 -0800

    Update utilities.cpp

--- lib/utilities.cpp
+++ lib/utilities.cpp
@@ -1,10 +1,10 @@
 namespace Utilities
 {
-    void ClampI(int* numberToClamp,int minValue, int maxValue){
-        if (*numberToClamp < minValue){ *numberToClamp = minValue; } else if (*numberToClamp > maxValue){*numberToClamp = maxValue;}
+    void ClampI(int numberToClamp,int minValue, int maxValue){
+        if ( numberToClamp < minValue){ return = minValue; } else if ( numberToClamp > maxValue){ return maxValue;} else {  return numberToClamp }
     }
     
-    void ClampF(float* numberToClamp, float minValue, float maxValue){
-        if (*numberToClamp < minValue){ *numberToClamp = minValue; } else if (*numberToClamp > maxValue){*numberToClamp = maxValue;}
+    void ClampF(float numberToClamp, float minValue, float maxValue){
+        if ( numberToClamp < minValue){ return minValue; } else if ( numberToClamp > maxValue){ return maxValue;} else {  return numberToClamp }
     }
 } // namespace Utilities
