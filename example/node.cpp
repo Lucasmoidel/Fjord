966ae1d59ab4360ad2fc03c51a2a5fc1d100a2ed
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2024-12-12 09:52:22 -0800

    Added paddle ai

--- example/node.cpp
+++ example/node.cpp
@@ -40,6 +40,7 @@ class Ball : public Node {
                     Lscore++;
                 }
             }
+            get_node("../Lpaddle")->transform.position.y += direction.y * Time::deltaTime * speed;
             //std::cout << Lscore << ", " << Rscore << std::endl;
         }
 };
@@ -52,8 +53,6 @@ void Start(){
     paddle1->side = 0;
     paddle2->side = 1;
     Ball* ball = engine.root.createNode<Ball>(300, 400, 30, 30, "Ball");
-
-    std::cout << ball->get_node("../Lpaddle")->name << "\n";
 
 }
 
