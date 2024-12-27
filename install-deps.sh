6ef69e8cb8c7ec5f234219a4e4d393e11f5aa826
Lucas Moidel <lucas122133@gmail.com>
2024-12-27 12:17:12 -0800

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -3,9 +3,12 @@
 # IF YOUR DISTRO's PAKAGE MANAGER IS NOT ON HERE THE FEEL GREE TO ADD IT
 
 set -e
+
+UNKNOWN="echo your distro is not on this list, feel free to add it. The needed pakages are sdl2, jsoncpp, yamlcpp, box2d, and cmake"
+
 PACMAN="sudo pacman -S sdl2 sdl2_ttf jsoncpp yaml-cpp box2d cmake"
 APT="sudo apt install libsdl2-dev jsoncpp yaml-cpp box2d cmake"
-DNF="echo we do not use fedora so we do not know the proper pakages to install. the pakages we use are sdl2, jsoncpp, yamlcpp, box2d, and cmake"
+DNF=$UNKNOWN
 
 if [ $(lsb_release -is) == "Arch" ]
 then
@@ -19,4 +22,7 @@ elif [ $(lsb_release -is) == "fedora" ]
 then
     echo $(lsb_release -is)
     $DNF
+else
+    echo $(lsb_release -is)
+    $UNKNOWN
 fi
