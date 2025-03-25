d9a6b0d30affb0f925a22148f9556815ee2cc0d2
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2025-03-25 10:31:11 -0700

    gl

--- lib/renderer.cpp
+++ lib/renderer.cpp
@@ -1,8 +1,15 @@
 #include "./Fjord.h"
 
+GLuint Renderer::compileShaders(){
+    
+}
+
 void Renderer::render(){
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+
+    
 
     // Swap buffers
     SDL_GL_SwapWindow(engine.window);
