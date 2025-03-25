553d602c8c0b3fc664928455a9fa3d668bd44c3f
Lucas Moidel <lucas122133@gmail.com>
2025-03-25 13:15:45 -0700

    error logging

--- lib/engine.cpp
+++ lib/engine.cpp
@@ -2,23 +2,44 @@
 
 // Create an SDL window with OpenGL
 // NOTE: Please add error handling later!
+
 bool Engine::create_window(std::string title, Vector2 size){
-    SDL_Init(SDL_INIT_VIDEO);
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("init sdl failed\n");
+        return false;
+    }
 
     window = SDL_CreateWindow(title.c_str(), size.x, size.y, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     
+    if (!window) {
+        printf ("failed to create window\n");
+        return false;
+    }
     // Create the OpenGL context
     glContext = SDL_GL_CreateContext(window);
 
+
+    if (!glContext) {
+        printf ("failed to create glContext\n");
+        return false;
+    }
     // Set OpenGL attributes for SDL 3
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    glewExperimental = GL_TRUE; // Ensure modern OpenGL extensions are loaded
-    GLenum err = glewInit();
+	glewExperimental = GL_TRUE;
+	GLenum glewError = glewInit();
+    if (glewInit() != GLEW_OK) {
+		std::cout << "Error initializing GLEW = " << glewGetErrorString(glewError) << std::endl;
+		return false;
+	}
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    return 1;
+    SDL_GL_SetSwapInterval(1);
+
+    return true;
 }
 
 bool Engine::destroy_window(){
